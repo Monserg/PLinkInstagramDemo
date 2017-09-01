cb4fbea93275fa35c70a4732f817474d18a88dad
SerhiiM <msm72@mail.ru>
2017-09-01 19:41:22 +0300

    add Instagram API: login

---
+++ PLinkInstagramDemo/Common/Models/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  PLinkInstagramDemo
+//
+//  Created by msm72 on 01.09.17.
+//  Copyright © 2017 RemoteJob. All rights reserved.
+//
+
+import Foundation
+
+struct INSTAGRAM_IDS {
+    // MARK: - Properties
+    static let INSTAGRAM_AUTHURL        =   "https://api.instagram.com/oauth/authorize/"
+    static let INSTAGRAM_APIURl         =   "https://api.instagram.com/v1/users/"
+    static let INSTAGRAM_CLIENT_ID      =   "e0ef0965580247e698a00c7a9fcecf07"
+    static let INSTAGRAM_CLIENTSERCRET  =   "8d744dc23f8543c1985663e54f3cd3e9"
+    static let INSTAGRAM_REDIRECT_URI   =   "http://plink.com.ua"
+    static let INSTAGRAM_ACCESS_TOKEN   =   "access_token"
+    static let INSTAGRAM_SCOPE          =   "likes+comments+relationships"
+}
